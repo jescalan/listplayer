135a20193fc81dc429fc880bd4a777027150b270
Jeff Escalante <gh.je@mailhero.io>
2016-06-08 18:40:40 -0400

    add commonjs export because webpack

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const babel = require('gulp-babel')
 const uglify = require('gulp-uglify')
 const umd = require('gulp-wrap-umd')
 const concat = require('gulp-concat')
+const change = require('gulp-change')
 
 gulp.task('build', () => {
   gulp.src(['src/emitter.js', 'src/index.js'])
@@ -21,4 +22,15 @@ gulp.task('min', () => {
     .pipe(gulp.dest('dist'))
 })
 
-gulp.task('default', ['build', 'min'])
+gulp.task('cjs', () => {
+  gulp.src(['src/emitter.js', 'src/index.js'])
+    .pipe(concat('listplayer.cjs.js'))
+    .pipe(babel({ presets: ['es2015'] }))
+    .pipe(change((content) => {
+      content += '\nmodule.exports = ListPlayer'
+      return content
+    }))
+    .pipe(gulp.dest('dist'))
+})
+
+gulp.task('default', ['build', 'min', 'cjs'])
